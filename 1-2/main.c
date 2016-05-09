0c09fe5debaca4ee05b1f795948a20d0df15099e
James Olds <git@jamesolds.me>
2016-05-09 12:30:24 -0500

    section excersises

--- 1-2/main.c
+++ 1-2/main.c
@@ -9,9 +9,10 @@ main() {
 	step = 20;
 
 	fahr = lower;
+	printf("Fahrenheit\tCelsius\n");
 	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32.0);
-		printf("%3.0f %6.1f\n", fahr, celsius);
+		printf("%3.0f %18.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
--- 1-2/main.rs
+++ 1-2/main.rs
@@ -2,9 +2,10 @@ fn main() {
 	let (lower, upper, step) = (0.0, 300.0, 20.0);
 
 	let mut fahr = lower;
+	println!("Fahrenheit {:^18}", "Celsius");
 	while fahr <= upper {
 		let celsius = format!("{:.*}", 1, ((5.0/9.0) * (fahr-32.0)));
-		println!("{:>3} {:>6}", fahr, celsius);
+		println!("{:>3} {:>18}", fahr, celsius);
 		fahr += step;
 	}
 }
